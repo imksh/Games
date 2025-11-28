7355e6360a121795355e19c79774b3450d3a8bc0
Karan Sharma <karan03945@gmail.com>
2025-11-29 03:48:25 +0530

    responsiveness added

--- src/pages/Ludo.jsx
+++ src/pages/Ludo.jsx
@@ -158,10 +158,10 @@ const Ludo = () => {
     }
   };
   return (
-    <div className="container min-h-screen min-w-screen flex flex-col justify-center items-center gradient1">
-      <div className="nav py-4 px-10 bg-blue-800 fixed top-0 w-full text-white flex justify-between items-center">
+    <div className="container min-h-screen min-w-screen flex flex-col justify-center items-center gradient1 overflow-hidden">
+      <div className="nav z-99 py-4 px-2 lg:px-10 bg-blue-800 fixed top-0 w-full text-white flex justify-between items-center">
         <h1 className="text-2xl">Snake & Ladder</h1>
-        <div className="flex gap-8 items-center justify-center">
+        <div className="flex gap-2 lg:gap-8 items-center justify-center">
           <button
             className={`px-5 py-1  rounded text-white cursor-pointer ${
               gameStarted ? "bg-gray-400" : "bg-green-500"
@@ -182,9 +182,9 @@ const Ludo = () => {
           </button>
         </div>
       </div>
-      <div className="flex min-w-screen justify-center items-center mt-15">
+      <div className="flex flex-col lg:flex-row min-w-screen justify-center items-center mt-15">
         {winner && (
-          <div className="absolute bg-green-600 text-white w-[50vw] h-[50vh] z-10 rounded-2xl flex justify-center gap-8 items-center flex-col z-50">
+          <div className="absolute bg-green-600 text-white w-[50vw] h-[50vh]  rounded-2xl flex justify-center gap-8 items-center flex-col z-50">
             <h1 className="text-4xl bold">{winner} is Winner</h1>
             <button
               className={`px-5 py-1  rounded text-white cursor-pointer z-50 bg-red-500`}
@@ -197,7 +197,7 @@ const Ludo = () => {
             </div>
           </div>
         )}
-        <div className="player1 flex-1 flex justify-center items-center">
+        <div className="player1 flex-1 hidden lg:flex justify-center items-center">
           <div className="flex flex-col shadow  justify-center items-center rounded-2xl w-[80%] gap-3 p-5">
             <input
               type="text"
@@ -232,49 +232,49 @@ const Ludo = () => {
               <img
                 src="/images/ludo/ladder.png"
                 alt="ladder"
-                className="absolute z-20 w-30 top-23"
+                className="absolute z-20 w-16 top-13 lg:w-30 lg:top-23"
               />
               <img
                 src="/images/ludo/ladder.png"
                 alt="ladder"
-                className="absolute z-20 w-30 h-48 top-77 left-58"
+                className="absolute z-20 w-20 h-23 top-46 left-31 lg:w-30 lg:h-48 lg:top-77 lg:left-58"
               />
               <img
                 src="/images/ludo/ladder.png"
                 alt="ladder"
-                className="absolute z-20 w-30 h-75 top-35 left-82 rotate-x-180 rotate-180"
+                className="absolute z-20 w-20 h-42 top-21 left-46 lg:w-30 lg:h-75 lg:top-35 lg:left-82 rotate-x-180 rotate-180"
               />
 
               <img
                 src="/images/ludo/ladder.png"
                 alt="ladder"
-                className="absolute z-20 w-30 h-45 top-10 right-0"
+                className="absolute z-20 w-20 h-23 top-5 -right-2 lg:w-30 lg:h-45 lg:top-10 lg:right-0"
               />
 
               <img
                 src="/images/ludo/ladder.png"
                 alt="ladder"
-                className="absolute z-20 w-33 h-40 top-59 left-18 rotate-x-180 rotate-180 rotate-z-12"
-              />
-
-              <img
-                src="/images/ludo/snake.png"
-                alt="snake"
-                className="absolute z-30 w-30 top-8 left-17"
+                className="absolute z-20 w-24 h-21 top-35 left-7 lg:w-33 lg:h-40 lg:top-59 lg:left-18 rotate-x-180 rotate-180 rotate-z-12"
               />
 
               <img
                 src="/images/ludo/snake.png"
                 alt="snake"
-                className="absolute z-30 h-35 top-48 left-20"
+                className="absolute z-30 w-23 top-3 left-8 lg:w-30 lg:top-8 lg:left-17"
+              />
+
+              <img
+                src="/images/ludo/snake.png"
+                alt="snake"
+                className="absolute z-30 h-20 top-28 left-10 lg:h-35 lg:top-48 lg:left-20"
               />
 
               <div
                 key={n}
-                className="w-14 h-14 border border-black relative flex justify-center items-center gap-2"
+                className="w-8 h-8 lg:w-14 lg:h-14 border border-black relative flex justify-center items-center gap-2"
               >
                 <div
-                  className={`z-40 absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] ${
+                  className={`z-30 absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] ${
                     (p1 === n || p2 === n) && p1 !== p2 ? "text-white" : ""
                   }`}
                 >
@@ -283,14 +283,18 @@ const Ludo = () => {
                 {p1 === n && (
                   <div
                     className={`${
-                      p1 === p2 ? "w-5 h-5" : "w-7 h-7"
+                      p1 === p2
+                        ? "w-3 h-3 lg:w-5 lg:h-5"
+                        : "w-5 h-5 lg:w-7 lg:h-7"
                     } bg-red-500 rounded-full`}
                   ></div>
                 )}
                 {p2 === n && (
                   <div
                     className={`${
-                      p1 === p2 ? "w-5 h-5" : "w-7 h-7"
+                      p1 === p2
+                        ? "w-3 h-3 lg:w-5 lg:h-5"
+                        : "w-5 h-5 lg:w-7 lg:h-7"
                     } bg-green-500 rounded-full`}
                   ></div>
                 )}
@@ -298,7 +302,7 @@ const Ludo = () => {
             </>
           ))}
         </div>
-        <div className="player1 flex-1 flex justify-center items-center">
+        <div className="player1 flex-1 hidden lg:flex justify-center items-center">
           <div className="flex flex-col shadow  justify-center items-center rounded-2xl w-[80%] gap-3 p-5">
             <input
               type="text"
@@ -318,6 +322,65 @@ const Ludo = () => {
             />
             <button
               className={`px-5 py-2 mt-4 rounded text-white cursor-pointer ${
+                !gameStarted || chance !== 2 ? "bg-gray-400" : " bg-blue-600"
+              }`}
+              id="btn1"
+              onClick={player2}
+              disabled={!gameStarted || chance === 1}
+            >
+              Roll Dice
+            </button>
+          </div>
+        </div>
+
+        <div className="small flex lg:hidden justify-around items-center my-5">
+          <div className="flex flex-col shadow  justify-center items-center rounded-2xl w-[40%] gap-3 px-5 py-4">
+            <input
+              type="text"
+              onChange={(e) => {
+                setPlayer1Name(e.target.value);
+              }}
+              value={player1Name}
+              className="text-2xl border-0 text-center max-w-[120px]"
+            />
+            <img
+              src={`/images/ludo/${dice1}.png`}
+              alt="dice"
+              width="40px"
+              height="40px"
+              className="object-cover"
+              ref={dice1Ref}
+            />
+            <button
+              className={`px-3 py-2 mt-4 rounded text-white cursor-pointer ${
+                !gameStarted || chance !== 1 ? "bg-gray-400" : " bg-blue-600"
+              }`}
+              id="btn1"
+              onClick={player1}
+              disabled={!gameStarted || chance === 2}
+            >
+              Roll Dice
+            </button>
+          </div>
+          <div className="flex flex-col shadow  justify-center items-center rounded-2xl w-[40%] gap-3 px-5 py-4">
+            <input
+              type="text"
+              onChange={(e) => {
+                setPlayer2Name(e.target.value);
+              }}
+              value={player2Name}
+              className="text-2xl border-0 text-center  max-w-[120px]"
+            />
+            <img
+              src={`/images/ludo/${dice2}.png`}
+              alt="dice"
+              width="40px"
+              height="40px"
+              className="object-cover"
+              ref={dice2Ref}
+            />
+            <button
+              className={`px-3 py-2 mt-4 rounded text-white cursor-pointer ${
                 !gameStarted || chance !== 2 ? "bg-gray-400" : " bg-blue-600"
               }`}
               id="btn1"
